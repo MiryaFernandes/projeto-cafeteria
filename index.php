8b082226fe538fc5dee0edc6068c26ab8939c6bf
miryã <db220355>
2022-03-15 16:55:18 -0300

    inicialização 15-03

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>grão de café</title>
+    <link rel="stylesheet" href="./css/style.css">   
+    <link rel="stylesheet" href="all.min.css">
+</head>
+<body>
+     
+    <div class="conteinar">
+    <span class="openMenu"><i class="fas fa-bars"></i></span>
+    <div class="sideMenu">
+    </div>
+        <span class="closeMenu"></span>
+    </div>
+</body>
+</html>
