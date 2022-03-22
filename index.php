1f2e82502ac650ee7e7f5fdc7565096cffb7adf7
miryã <db220355>
2022-03-22 16:13:08 -0300

    segunda tentativa do projeto

--- index.php
+++ index.php
@@ -4,16 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>grão de café</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">   
     <link rel="stylesheet" href="all.min.css">
 </head>
-<body>
-     
-    <div class="conteinar">
-    <span class="openMenu"><i class="fas fa-bars"></i></span>
-    <div class="sideMenu">
-    </div>
-        <span class="closeMenu"></span>
-    </div>
+<body>  
+    <header>    
+        <nav>
+            <a href="" class="logo">grãos de cafe</a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="/">produtos</a></li>
+                <li><a href="/">quem somos</a></li>
+                <li><a href="/">estabelecimentos</a></li>
+                <li><a href="/">parcerias</a></li>
+                </ul>
+        </nav>
+    </header>        
+    <main>
+        <div class="main">
+            
+            <div class="imagem">
+                <img src="./img/pumpkin-spice.png" alt="">
+            </div>
+
+            <div class="informacoes">
+                <h1>pumpkin chai latte - 150ml</h1>
+
+            <div class="dealhes">
+                compra somente no estabelecimento
+            </div>
+
+            <div class="preco">
+                <h2>R$14,25</h2>
+            </div>
+        </div>
+        </div>
+
+    </main>
+    <script src="mobile-navbar.js"></script>
 </body>
 </html>
