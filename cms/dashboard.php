2dd3eb759be01c56b0b518e9fc3d38397f6e966d
miryã <db220355>
2022-03-29 16:53:09 -0300

    login completo e dashboard pela metade

---
+++ cms/dashboard.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+            <nav class="titulo-site">
+                <h1>CMS Grãos de Café</h1>
+                <h2>gerenciamento do conteudo do site</h2>
+            </nav>
+    </header>
+    <main>
+        <nav class="selecao">
+            <div class="itens">
+                <div class="produtos">
+                    <img src="../icons/list.png">
+                    <a href="">adm. produtos</a>
+                </div>
+
+                <div class="categorias">
+                    <img src="../icons/category-regular.png">
+                    <a href="">adm. de categorias</a>
+                </div>
+
+                <div class="categorias">
+                    <img src="../icons/contact.png">
+                    <a href="">contatos</a>
+                </div>
+
+                <div class="categorias">
+                    <img src="../icons/user-pin.png">
+                    <a href="">usuarios</a>
+                </div>
+            </div>
+            <nav class="logout">
+                <h3>bem vindo</h3>
+                <h3>logout</h3>
+            </nav>
+
+        </nav>
+
+        <nav>
+            <h1>titulo da sessão</h1>
+        </nav>
+    </main>
+
+    <footer>
+        <footer>copyright 2022® | Miryã Elizabeth</footer>
+    </footer>
+</body>
+</html>
---
+++ cms/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cms.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <section class="area-login">
+        <div class="login">
+            <div>
+                <img src="../img/donut.png">
+            </div>
+
+            <form method="POST">
+                <input type="text" name="nome" placeholder="nome de usuario" autofocus>
+                <input type="password" name="senha" placeholder="senha" autofocus="sua senha">
+                <input type="submit" value="entrar">
+            </form>
+            <P> ainda não tem uma conta?<a href="#">criar uma conta</a></P>
+        </div>      
+    </section>
+</body>
+</html>
